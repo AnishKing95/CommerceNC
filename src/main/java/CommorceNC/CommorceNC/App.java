3bd5c7df9b30653c996189d6c9641330901e58da
ADMIN <ADMIN@DESKTOP-JLQ4OT0>
2023-01-20 10:59:42 +0530

    First commit

---
+++ src/main/java/CommorceNC/CommorceNC/App.java
@@ -0,0 +1,13 @@
+package CommorceNC.CommorceNC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
