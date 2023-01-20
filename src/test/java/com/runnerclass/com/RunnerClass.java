d1f323e12f0fc6c84d5ca24972c2dbe2c400ea8d
NEW <NEW@DELL>
2023-01-20 16:24:11 +0530

    creating the runner class

---
+++ src/test/java/com/runnerclass/com/RunnerClass.java
@@ -0,0 +1,12 @@
+package com.runnerclass.com;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+public class RunnerClass {
+
+}
